9b167b352e4cceb4189a672c2f5889b674d4635e
ronald <89151207+ronaldhsp@users.noreply.github.com>
2022-12-19 12:58:14 +0800

    Add 16x16 grid of squares

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const container = document.querySelector(".grid-container");
+const containerHeight = container.clientWidth;
+const containerWidth = container.clientHeight;
+
+const smallerLength = containerHeight > containerWidth ? containerHeight : containerWidth;
+const squareTotalSide = smallerLength / 16;
+const squareContentSide = squareTotalSide - 4;  // remove margin
+
+const marginBetweenSquares = 2;  // px
+const numberOfSquares = 16 * 16;
+for (let i = 0; i < numberOfSquares; i++) {
+  const square = document.createElement("div");
+  square.style.height = `${squareContentSide}px`;
+  square.style.width = `${squareContentSide}px`;
+  square.style.margin = `${marginBetweenSquares}px`;
+  square.style.border = "1px solid red";
+  square.style.cssFloat = "left";
+  container.appendChild(square);
+}
