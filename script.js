b725d1bd80adfc4cb0fc5eb7c5cc4ca13f95d88f
ronald <89151207+ronaldhsp@users.noreply.github.com>
2022-12-19 13:27:30 +0800

    Add button to change grid size

--- script.js
+++ script.js
@@ -4,25 +4,46 @@ function changeColor(event) {
   event.srcElement.style.backgroundColor = "red";
 }
 
-const container = document.querySelector(".grid-container");
-const containerHeight = container.clientWidth;
-const containerWidth = container.clientHeight;
+function drawGrid(numberOfSquaresPerSide) {
+  const container = document.querySelector(".grid-container");
+  const containerHeight = container.clientWidth;
+  const containerWidth = container.clientHeight;
+  const marginBetweenSquares = 2;  // px
 
-const smallerLength = containerHeight > containerWidth ? containerHeight : containerWidth;
-const squareTotalSide = smallerLength / 16;
-const squareContentSide = squareTotalSide - 4;  // remove margin
+  const smallerLength = containerHeight > containerWidth ? containerHeight : containerWidth;
+  const squareTotalSide = smallerLength / numberOfSquaresPerSide;
+  const squareContentSide = squareTotalSide - 2 * marginBetweenSquares;  // remove margin
 
-const marginBetweenSquares = 2;  // px
-const numberOfSquares = 16 * 16;
-for (let i = 0; i < numberOfSquares; i++) {
-  const square = document.createElement("div");
-  square.style.height = `${squareContentSide}px`;
-  square.style.width = `${squareContentSide}px`;
-  square.style.margin = `${marginBetweenSquares}px`;
-  square.style.border = "1px solid red";
-  square.style.cssFloat = "left";
+  const numberOfSquares = numberOfSquaresPerSide * numberOfSquaresPerSide;
+  for (let i = 0; i < numberOfSquares; i++) {
+    const square = document.createElement("div");
+    square.style.height = `${squareContentSide}px`;
+    square.style.width = `${squareContentSide}px`;
+    square.style.margin = `${marginBetweenSquares}px`;
+    square.style.border = "1px solid red";
+    square.style.cssFloat = "left";
 
-  square.addEventListener("mouseenter", changeColor, {once: true})
+    square.addEventListener("mouseenter", changeColor, {once: true})
 
-  container.appendChild(square);
+    container.appendChild(square);
+  }
 }
+
+function clearGrid() {
+  const container = document.querySelector(".grid-container");
+  container.innerHTML = "";
+}
+
+function askForGridSize() {
+  const gridSize = +prompt("Number of squares per side? [1-100]", 16);
+  if (!Number.isInteger(gridSize) || gridSize < 1 || gridSize > 100) {
+    alert("Invalid input!");
+    return;
+  }
+  clearGrid();
+  drawGrid(gridSize);
+}
+
+drawGrid(16);
+const button = document.querySelector("button");
+button.addEventListener("click", askForGridSize);
