b358c0847e7ec9838917ef86dcf745cf03cbbae2
ronald <89151207+ronaldhsp@users.noreply.github.com>
2022-12-19 13:39:19 +0800

    Add button to reset grid

--- script.js
+++ script.js
@@ -8,18 +8,15 @@ function drawGrid(numberOfSquaresPerSide) {
   const container = document.querySelector(".grid-container");
   const containerHeight = container.clientWidth;
   const containerWidth = container.clientHeight;
-  const marginBetweenSquares = 2;  // px
 
   const smallerLength = containerHeight > containerWidth ? containerHeight : containerWidth;
-  const squareTotalSide = smallerLength / numberOfSquaresPerSide;
-  const squareContentSide = squareTotalSide - 2 * marginBetweenSquares;  // remove margin
+  const squareContentSide = smallerLength / numberOfSquaresPerSide;
 
   const numberOfSquares = numberOfSquaresPerSide * numberOfSquaresPerSide;
   for (let i = 0; i < numberOfSquares; i++) {
     const square = document.createElement("div");
     square.style.height = `${squareContentSide}px`;
     square.style.width = `${squareContentSide}px`;
-    square.style.margin = `${marginBetweenSquares}px`;
     square.style.border = "1px solid red";
     square.style.cssFloat = "left";
 
@@ -34,16 +31,24 @@ function clearGrid() {
   container.innerHTML = "";
 }
 
-function askForGridSize() {
-  const gridSize = +prompt("Number of squares per side? [1-100]", 16);
-  if (!Number.isInteger(gridSize) || gridSize < 1 || gridSize > 100) {
-    alert("Invalid input!");
-    return;
-  }
+function resetGrid() {
   clearGrid();
   drawGrid(gridSize);
 }
 
-drawGrid(16);
-const button = document.querySelector("button");
-button.addEventListener("click", askForGridSize);
+function askForGridSize() {
+  gridSize = +prompt("Number of squares per side? [1-100]", 16);
+  if (!Number.isInteger(gridSize) || gridSize < 1 || gridSize > 100) {
+    alert("Invalid input!");
+    return;
+  }
+  resetGrid();
+}
+
+let gridSize = 16;
+drawGrid(gridSize);
+
+const inputButton = document.querySelector(".input-btn");
+inputButton.addEventListener("click", askForGridSize);
+const resetButton = document.querySelector(".reset-btn");
+resetButton.addEventListener("click", resetGrid);
